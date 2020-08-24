5ed90bb1a969b6789c69800f0927a61d18b7a9dc
Yirui Wu <wu.yi.rui.91@gmail.com>
2020-08-25 01:32:54 +0800

    Added DisbursementListActivity and DisbursementListActivity.java Layout file

--- app/src/main/java/iss/workshop/adprojectmobile/activity/DisbursementListActivity.java
+++ app/src/main/java/iss/workshop/adprojectmobile/activity/DisbursementListActivity.java
@@ -1,19 +1,18 @@
 package iss.workshop.adprojectmobile.activity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.widget.TextView;
 import iss.workshop.adprojectmobile.R;
 
 public class DisbursementListActivity extends AppCompatActivity {
-TextView collectionInfo;
+    TextView collectionInfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disbursement_list);
-         collectionInfo =findViewById(R.id.collectionPointDisbursementInfo);
+        collectionInfo =findViewById(R.id.collectionPointDisbursementInfo);
 
         //Test Data
         String cInfo="Collection Date: 09/08/2020"+ System.lineSeparator()+
